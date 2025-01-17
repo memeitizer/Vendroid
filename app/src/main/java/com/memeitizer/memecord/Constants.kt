c4b4697c420319492a472378df76b30887e55ac6
memeitizer <memeitizerlimited@gmail.com>
2025-01-17 11:27:53 +0000

    Update and rename Constants.kt to Constants.kt

---
+++ app/src/main/java/com/memeitizer/memecord/Constants.kt
@@ -0,0 +1,6 @@
+package com.memeitizer.memecord
+
+object Constants {
+    const val JS_BUNDLE_URL = "https://github.com/memeitizer/plugin/releases/download/devbuild/browser.js"
+    const val EQUICORD_BUNDLE_URL = "https://github.com/memeitizer/plugin/releases/download/devbuild-equi/browser.js"
+}
